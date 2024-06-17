b1e8649e06f60f3c9147821529f75cea2aad9301
Fyras Telmini <69505085+fyrastelmini@users.noreply.github.com>
2024-06-17 02:21:54 +0000

    added download_csv route

--- calculator_app/calculator_app.py
+++ calculator_app/calculator_app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Request, Form, Response
 from fastapi.templating import Jinja2Templates
 from utilities import calculate_rpn, insert_into_db, view_database
 
@@ -27,3 +27,12 @@ async def calculate(request: Request, text: str = Form(...)):
 def view_data(request: Request):
     data = view_database()
     return templates.TemplateResponse("view_data.html", {"request": request, "data": data})
+
+@app.get("/download_csv")
+def download_csv():
+    data = view_database()
+    csv = "opération,résultat,date\n"
+    for row in data:
+        print(row)
+        csv += f"{row['operation']},{row['result']},{row['datetime']}\n"
+    return Response(content=csv, media_type="text/csv", headers={"Content-Disposition": "attachment; filename=data.csv"})
